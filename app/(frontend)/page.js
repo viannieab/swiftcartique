191f5d8fb73aca3dc73b401e28c929b16193dcc5
viannieab <viannie.edwin@gmail.com>
2024-04-05 17:58:16 +0300

    added auth

--- app/(frontend)/page.js
+++ app/(frontend)/page.js
@@ -1,15 +1,18 @@
-import CategoryList from "@/components/frontend/CategoryList";
-import CommunityList from "@/components/frontend/CommunityList";
-import Hero from "@/components/frontend/Hero";
-import MarketList from "@/components/frontend/MarketList";
-import { getData } from "@/lib/getData";
-import Link from "next/link"
+import CategoryList from "@/components/frontend/CategoryList"
+import CommunityList from "@/components/frontend/CommunityList"
+import Hero from "@/components/frontend/Hero"
+import MarketList from "@/components/frontend/MarketList"
+import { authOptions } from "@/lib/authOptions"
+import { getData } from "@/lib/getData"
+import { getServerSession } from "next-auth"
 
 export default async function Home() {
   const categoriesData = await getData('categories')
   const categories = categoriesData.filter((category)=>{
     return category.products.length > 3
   })
+  const session = await getServerSession(authOptions)
+  console.log(session?.user)
   return (
     <div className="min-h-screen">
        <Hero/>
--- context/Providers.jsx
+++ context/Providers.jsx
@@ -6,6 +6,7 @@ import { extractRouterConfig } from "uploadthing/server"
 import { ourFileRouter } from "../app/api/uploadthing/core"
 import { Provider } from "react-redux"
 import { store } from "@/redux/store"
+import { SessionProvider } from "next-auth/react"
 
 export default function Providers({children}){
     return (
@@ -14,9 +15,11 @@ export default function Providers({children}){
                 routerConfig={extractRouterConfig(ourFileRouter)}
             />
             <Toaster position="top-center" reverseOrder={false}/>
-            <Provider store={store}>
-                {children}
-            </Provider>
+            <SessionProvider>
+                <Provider store={store}>
+                    {children}
+                </Provider>
+            </SessionProvider>
         </ThemeProvider>
     )
 }
