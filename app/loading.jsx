07ff9897cdd03fca2c0990ae0f40ad8355bb8e5a
Viannieab <viannie.edwin@gmail.com>
2024-06-04 11:44:20 +0300

    feat: changed loading indicator

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Loading() {
   return (
-    <div>
-        <p>Loading...</p>
+    <div className='absolute top-0 bottom-0 right-0 left-0 z-[230] bg-white flex justify-center items-center'>
+      <div className='spinner'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
